c7dd6a3401dd82297917cccfb32bab848578d645
Lionel1999 <980980068@qq.com>
2022-05-24 11:04:37 +0800

    LC001_One-Pass_Hash

---
+++ src/main/cpp/gsg/LeetCode/LC0001_Two_Sum.cc
@@ -0,0 +1,27 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> valueToKey;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = valueToKey.find(target - nums[i]);
+            if (it != valueToKey.cend())
+            {
+                return {it->second, i};
+            }
+            else
+            {
+                valueToKey[nums[i]] = i;
+            }
+        }
+
+        return {};
+    }
+};
