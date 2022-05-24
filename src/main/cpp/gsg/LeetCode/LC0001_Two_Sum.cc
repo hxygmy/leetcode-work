e4114dd29256d62bd7246c765a8e41765670f0c7
Lionel1999 <980980068@qq.com>
2022-05-24 11:04:37 +0800

    LC001_One-Pass_Hash

--- src/main/cpp/gsg/LeetCode/LC0001_Two_Sum.cc
+++ src/main/cpp/gsg/LeetCode/LC0001_Two_Sum.cc
@@ -3,36 +3,6 @@
 
 using namespace std;
 
-// 2. Two-Pass Hash Table
-// 方便变形拓展
-// 最直白的思路直接翻译
-class Solution
-{
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        unordered_map<int, int> memo;
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            memo[nums[i]] = i;
-        }
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            if (memo.count(target - nums[i]))
-            {
-                int idx = memo[target - nums[i]];
-                if (idx != i)
-                {
-                    return {i, idx};
-                }
-            }
-        }
-        return {};
-    }
-};
-
-// 1. One-Pass Hash Table
-// 特题特解，最好写，也最快
 class Solution
 {
 public:
