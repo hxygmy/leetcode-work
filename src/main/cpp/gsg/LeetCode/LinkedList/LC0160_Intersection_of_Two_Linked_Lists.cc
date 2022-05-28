d278035d91e720402002ed01e0e1f65ba9efc524
Lionel1999 <980980068@qq.com>
2022-05-28 09:39:21 +0800

    Never forget Brute Force.

--- src/main/cpp/gsg/LeetCode/LinkedList/LC0160_Intersection_of_Two_Linked_Lists.cc
+++ src/main/cpp/gsg/LeetCode/LinkedList/LC0160_Intersection_of_Two_Linked_Lists.cc
@@ -5,6 +5,8 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
+// 3. 反而忘了最朴素的 set 判重方法，这里留一句话记着
+
 // 2. 双指针分别遍历双链表（艺术）
 // 被 clangformatter 改了缩进，没那么艺术了，不如再多加点换行清晰一点
 // 就这样吗，要看这个代码不如去再写一遍爽一爽
