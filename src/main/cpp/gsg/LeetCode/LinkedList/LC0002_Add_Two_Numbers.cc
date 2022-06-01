f4e8c6ff2b5c0eb3d7d1d1dfc1618a2a1a45a743
Lionel1999 <980980068@qq.com>
2022-06-01 14:41:09 +0800

    LC0002 upgrade

--- src/main/cpp/gsg/LeetCode/LinkedList/LC0002_Add_Two_Numbers.cc
+++ src/main/cpp/gsg/LeetCode/LinkedList/LC0002_Add_Two_Numbers.cc
@@ -7,9 +7,65 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+// 3. 原地操作，只申请一个 dummy 和 末尾进位必须申请的 tail原地操作
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *dummy = new ListNode(0);
+        ListNode *p1 = l1; // p1 当基
+        ListNode *p2 = l2;
+        // 设两个链表均不为空
+        ListNode *tail = p1; // 不多这一个指针，多一个指针更好写，更好读
+        dummy->next = tail;
+        tail->val = p1->val + p2->val;
+        p1 = p1->next;
+        p2 = p2->next;
+
+        while (p1 && p2)
+        {
+            if (tail->val >= 10)
+            {
+                tail->val -= 10;
+                p1->val += 1;
+            }
+            p1->val += p2->val;
+            tail->next = p1;
+            tail = p1;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+
+        ListNode *remain = p1 ? p1 : p2;
+        while (remain)
+        {
+            if (tail->val >= 10)
+            {
+                tail->val -= 10;
+                remain->val += 1;
+            }
+            tail->next = remain;
+            tail = remain;
+            remain = remain->next;
+        }
+        if (tail->val >= 10)
+        {
+            tail->val -= 10;
+            ListNode *newNode = new ListNode(0);
+            newNode->val = 1;
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        return dummy->next;
+    }
+};
+
 // 2. 修改自下面的自作聪明构造 顺序链表 的解，
 //    写的比较丑，但也90%，暂时没心情整理了，先搁这吧，
 //    没想到能在这出岔子
+//    这里不应该再申请这么多新节点空间的，按题目要求很好写，见下一个解（数字上升）
 class Solution
 {
 public:
