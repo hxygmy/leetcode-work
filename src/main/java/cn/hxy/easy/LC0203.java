17afc1bea89377b4aaa4170e0ade50af0745df60
hexiaoyu <hexi.aoyu@163.com>
2022-06-15 18:30:47 +0800

    hxy: 203. 移除链表元素

---
+++ src/main/java/cn/hxy/easy/LC0203.java
@@ -0,0 +1,75 @@
+package cn.hxy.easy;
+
+/**
+ * 203. 移除链表元素
+ *
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,6,3,4,5,6], val = 6
+ * 输出：[1,2,3,4,5]
+ * 示例 2：
+ *
+ * 输入：head = [], val = 1
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [7,7,7,7], val = 7
+ * 输出：[]
+ *  
+ *
+ * 提示：
+ *
+ * 列表中的节点数目在范围 [0, 104] 内
+ * 1 <= Node.val <= 50
+ * 0 <= val <= 50
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/remove-linked-list-elements
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Applewald
+ * 2022/6/11 17:37
+ */
+public class LC0203 {
+	public static void main(String[] args) {
+
+	}
+
+	public ListNode removeElements(ListNode head, int val) {
+		ListNode res = new ListNode();
+		ListNode point = res;
+		res.next = head;
+		while (point.next != null) {
+			if (point.next.val == val) {
+				point.next = point.next.next;
+			} else {
+				point = point.next;
+			}
+		}
+		return res.next;
+	}
+
+
+	private class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
+
