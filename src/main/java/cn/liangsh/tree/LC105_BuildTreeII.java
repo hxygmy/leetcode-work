25f6a51a9061fe8811df989394ba705931efbf2e
Liangsh <liangshunhao@ebaolife.com>
2022-06-15 17:03:12 +0800

    day07:刷题！

--- src/main/java/cn/liangsh/tree/LC105_BuildTreeII.java
+++ src/main/java/cn/liangsh/tree/LC105_BuildTreeII.java
@@ -2,21 +2,18 @@ package cn.liangsh.tree;
 
 /**
  * @author Liangsh
- * @description 构建二叉树
- * @date 2022/6/15 13:44
+ * @description 二叉树建树 先序
+ * @date 2022/6/15 17:02
  */
-public class LC103_BuildTree {
+public class LC105_BuildTreeII {
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (inorder.length == 0 || postorder.length == 0) {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (inorder.length == 0 || preorder.length == 0) {
             return null;
         }
 
-        // 获取当前节点值,为后序数组最后一个元素
-        int rootVal = postorder[postorder.length - 1];
+        int rootVal = preorder[0];
         TreeNode root = new TreeNode(rootVal);
-
-        // 根据后序根节点划分中序数组
         int index = 0;
         for (int i = 0; i < inorder.length; i++) {
             if (inorder[i] == rootVal) {
@@ -25,21 +22,22 @@ public class LC103_BuildTree {
             }
         }
 
-        // 中序划分，左闭右开
         int[] leftInorder = new int[index];
-        // index为真实长度的前一位
         int[] rightInorder = new int[inorder.length - index - 1];
         System.arraycopy(inorder, 0, leftInorder, 0, index);
-        System.arraycopy(inorder, index, rightInorder, 0, inorder.length - index - 1);
+        System.arraycopy(inorder, index + 1, rightInorder, 0, inorder.length - index - 1);
 
-        // 后序数组长度和中序相同
-        int[] leftPostorder = new int[index];
-        int[] rightPostorder = new int[inorder.length - index - 1];
-        System.arraycopy(postorder, 0, leftPostorder, 0, index);
-        System.arraycopy(postorder, index, rightPostorder, 0, inorder.length - index - 1);
+        int[] leftPreorder = new int[index];
+        int[] rightPreorder = new int[inorder.length - index - 1];
+        System.arraycopy(preorder, 1, leftPreorder, 0, index);
+        System.arraycopy(preorder, index + 1, rightPreorder, 0, inorder.length - index - 1);
+
+        root.left = buildTree(leftPreorder, leftInorder);
+        root.right = buildTree(rightPreorder, rightInorder);
 
         return root;
     }
+
 
     class TreeNode {
         int val;
