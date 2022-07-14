e001f694230496df4544bf04ce108faf073448a0
hexiaoyu <hexi.aoyu@163.com>
2022-07-14 16:57:31 +0800

    hxy: 242. 有效的字母异位词

---
+++ src/main/java/cn/hxy/easy/LC0242.java
@@ -0,0 +1,53 @@
+package cn.hxy.easy;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * 242. 有效的字母异位词
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= s.length, t.length <= 5 * 104
+ * s 和 t 仅包含小写字母
+ *  
+ *
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Applewald
+ * 2022/7/14 15:35
+ */
+public class LC0242 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) return false;
+		char[] a = s.toCharArray();
+		Arrays.sort(a);
+		char[] b = t.toCharArray();
+		Arrays.sort(b);
+		return Arrays.equals(a, b);
+	}
+}
