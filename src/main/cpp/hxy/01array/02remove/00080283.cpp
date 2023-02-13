d1dc3cba5d3ea3205ca18e52edc9adf270d2a49c
hexiaoyu <hexi.aoyu@163.com>
2023-02-14 06:29:00 +0800

    C++ 0283

---
+++ src/main/cpp/hxy/01array/02remove/00080283.cpp
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/14 6:15
+ * https://leetcode.cn/problems/move-zeroes/
+ * easy
+ */
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if(nums[slow] != 0) {
+                slow++;
+            } else if (nums[slow] == 0 and nums[i] != 0){
+                int tmp = nums[i];
+                nums[i] = 0;
+                nums[slow++] = tmp;
+            }
+        }
+    }
+};
