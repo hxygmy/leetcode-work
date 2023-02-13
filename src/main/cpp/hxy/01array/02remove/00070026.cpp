eb274f242808da8aa3b34153b3170dfffa80a0d1
hexiaoyu <hexi.aoyu@163.com>
2023-02-14 06:15:25 +0800

    C++ 0026

---
+++ src/main/cpp/hxy/01array/02remove/00070026.cpp
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/14 6:01
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+ * easy
+ */
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow = 0;
+        int quick = 1;
+        int size = nums.size();
+        while(quick < size){
+            if(nums[slow] == nums[quick]){
+                quick++;
+            } else {
+                nums[++slow] = nums[quick++];
+            }
+        }
+        return slow + 1;
+    }
+};
