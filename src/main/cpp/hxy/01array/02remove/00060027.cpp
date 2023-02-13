c9944ecdfe2d456f359a4e9cbc947f441f18d8dd
hexiaoyu <hexi.aoyu@163.com>
2023-02-14 06:03:38 +0800

    C++ 0027

---
+++ src/main/cpp/hxy/01array/02remove/00060027.cpp
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/14 5:38
+ * https://leetcode.cn/problems/remove-element/
+ * easy
+ */
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0;
+        // 双指针法，i是快指针，left是慢指针
+        for (int i = 0; i < nums.size(); i++){
+            if(nums[i] != val) {
+                nums[left++] = nums[i];
+            }
+        }
+        return left;
+    }
+};
