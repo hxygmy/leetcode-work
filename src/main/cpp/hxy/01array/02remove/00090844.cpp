0ce71687ef18debaefbecbc0f52f39754da5c1d3
hexiaoyu <hexi.aoyu@163.com>
2023-02-14 23:06:14 +0800

    C++ 0844

---
+++ src/main/cpp/hxy/01array/02remove/00090844.cpp
@@ -0,0 +1,71 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/14 6:25
+ * https://leetcode.cn/problems/backspace-string-compare/
+ * easy
+ */
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+
+        // skip计数器
+        int s_skip = 0, t_skip = 0;
+        int sp = s.length() - 1, tp = t.length() - 1;
+
+        while(sp >= 0 or tp >= 0){
+            while (sp >= 0) {
+                if(s[sp] == '#') {
+                    s_skip++;
+                    sp--;
+                } else if (s_skip > 0) {
+                    s_skip--;
+                    sp--;
+                } else {
+                    break;
+                }
+            }
+
+            while (tp >= 0) {
+                if(t[tp] == '#') {
+                    t_skip++;
+                    tp--;
+                } else if(t_skip > 0) {
+                    t_skip--;
+                    tp--;
+                } else {
+                    break;
+                }
+            }
+
+            if (sp >= 0 and tp >= 0) {
+                if (s[sp] != t[tp]) {
+                    return false;
+                } else {
+                    sp--;
+                    tp--;
+                }
+            } else {
+                if (sp >= 0 or tp >= 0){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+        }
+
+        return true;
+    }
+};
+
+int main(){
+    string s = "ab##";
+    string t = "c#d#";
+    Solution *a = new Solution();
+    bool b = a->backspaceCompare(s, t);
+    return b;
+}
