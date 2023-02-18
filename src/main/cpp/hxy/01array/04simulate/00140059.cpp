7fd01f0b24a2bbce3b02385bb0a8b2dc01af8c36
hexiaoyu <hexi.aoyu@163.com>
2023-02-18 14:13:40 +0800

    C++ 0059 tmp

---
+++ src/main/cpp/hxy/01array/04simulate/00140059.cpp
@@ -0,0 +1,47 @@
+/*
+ * Created by hexiaoyu 
+ * on 2023/2/18 13:36
+ * https://leetcode.cn/problems/spiral-matrix-ii/
+ * medium
+ */
+
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        if (n == 1) return {{1}};
+
+        vector<vector<int>> res(n, vector<int>(n));
+
+        int i = 0;
+        int j = i;
+        int begin = 0;
+        int end = n - 1;
+
+        for (int k = 1; k <= n * n; k++) {
+            while(j < end) {
+                res[i][j] = k++;
+                j++;
+            }
+            while(i < end) {
+                res[i][j] = k++;
+                i++;
+            }
+            while(j > begin) {
+                res[i][j] = k++;
+                j--;
+            }
+            begin++;
+            while(i > begin) {
+                res[i][j] = k++;
+                i--;
+            }
+            end--;
+        }
+        res[i][j] = n * n;
+        return res;
+    }
+};
