cc5c8ce4756f690bd90942be5fb3d2bd0720b82e
hexiaoyu <hexi.aoyu@163.com>
2023-02-22 00:04:53 +0800

    C++ 0160

---
+++ src/main/cpp/hxy/02linked_list/00210160.cpp
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/21 23:38
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/
+ * easy
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a = headA, *b = headB;
+        while(a != b){
+            a = a? a->next : headB;
+            b = b? b->next : headA;
+        }
+        return a;
+    }
+};
