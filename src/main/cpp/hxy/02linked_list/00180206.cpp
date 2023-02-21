4fae9994733177832d4e83b8598d8b518410e00a
hexiaoyu <hexi.aoyu@163.com>
2023-02-21 22:55:50 +0800

    C++ 0206

---
+++ src/main/cpp/hxy/02linked_list/00180206.cpp
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/21 22:32
+ * https://leetcode.cn/problems/reverse-linked-list/
+ * easy
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return nullptr;
+        ListNode *p = new ListNode(0, head);
+        ListNode *tmp = p->next;
+        ListNode *t = tmp->next;
+        tmp->next = nullptr;
+        while(t){
+            tmp = t->next;
+            t->next = p->next;
+            p->next = t;
+            t = tmp;
+        }
+        return p->next;
+    }
+};
