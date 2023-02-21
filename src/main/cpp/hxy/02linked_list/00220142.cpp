4a4292667af3c91d516c7e1262194e2593762c35
hexiaoyu <hexi.aoyu@163.com>
2023-02-22 00:32:31 +0800

    C++ 0142

---
+++ src/main/cpp/hxy/02linked_list/00220142.cpp
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/22 0:05
+ * https://leetcode.cn/problems/linked-list-cycle-ii/
+ * medium
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        // 双指针，玩的是数学啊
+        ListNode *fast = head, *slow = head;
+        do {
+            if(!fast or !fast->next) return nullptr;
+            fast = fast->next->next;
+            slow = slow->next;
+        } while (fast != slow);
+        fast = head;
+        while (fast != slow) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
