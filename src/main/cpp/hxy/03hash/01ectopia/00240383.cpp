dd56d8e3b6ac407531c058c9e96595dccddacd6d
hexiaoyu <hexi.aoyu@163.com>
2023-02-23 01:34:28 +0800

    C++ 0383

---
+++ src/main/cpp/hxy/03hash/01ectopia/00240383.cpp
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/23 1:27
+ * https://leetcode.cn/problems/ransom-note/
+ * easy
+ */
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int res[26] = {0};
+        for(char c : magazine){
+            res[c - 'a']++;
+        }
+        for(char c : ransomNote) {
+            if(res[c - 'a'] == 0) return false;
+            res[c - 'a']--;
+        }
+        return true;
+    }
+};
