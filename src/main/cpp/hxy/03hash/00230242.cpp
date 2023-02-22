f1f85a73809872c0083c39a0cc8ec71b6d71a725
hexiaoyu <hexi.aoyu@163.com>
2023-02-23 01:27:23 +0800

    C++ 0242

---
+++ src/main/cpp/hxy/03hash/00230242.cpp
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/22 0:35
+ * https://leetcode.cn/problems/valid-anagram/
+ * easy
+ */
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // 定义一个含有26个英文字母的数组，遍历s以增加，遍历t以减少
+        int res[26] = {0};
+        for(char c : s) {
+            res[c - 'a']++;
+        }
+        for(char c : t) {
+            res[c - 'a']--;
+        }
+        for(int f : res) {
+            if(f != 0) return false;
+        }
+        return true;
+    }
+};
