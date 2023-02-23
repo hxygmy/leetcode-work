73301c5f88017ae267ef8d8ca4e68741a1950221
hexiaoyu <hexi.aoyu@163.com>
2023-02-24 02:14:43 +0800

    C++ 0541

---
+++ src/main/cpp/hxy/04string/00350541.cpp
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/24 1:56
+ * https://leetcode.cn/problems/reverse-string-ii/
+ * easy
+ */
+
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        char tmp;
+        int left, right;
+        for(int i = 0; i < s.size(); i += 2 * k){
+            left = i;
+            if(i + k - 1 < s.size()) {
+                right = i + k - 1;
+            } else {
+                right = s.size() - 1;
+            }
+            while(left < right){
+                tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
+
+int main(){
+    string s = "abcdefg";
+    int k = 2;
+    Solution *a = new Solution();
+    auto b = a->reverseStr(s, k);
+    return 0;
+}
