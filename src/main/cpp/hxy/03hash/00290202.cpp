d2bae706a359d91a3d1ef7014d353d59f52fb166
hexiaoyu <hexi.aoyu@163.com>
2023-02-23 17:58:35 +0800

    C++ 0202

---
+++ src/main/cpp/hxy/03hash/00290202.cpp
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/23 17:28
+ * https://leetcode.cn/problems/happy-number/
+ * easy
+ */
+#include "unordered_set"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        int p = n;
+        int times = 0;
+
+        unordered_set<int> flag;
+
+        while (true) {
+            if(flag.find(p) != flag.end()) return false;
+            flag.insert(p);
+            while (p != 0) {
+                times += (p % 10) * (p % 10);
+                p /= 10;
+            }
+            p = times;
+            times = 0;
+            if(p == 1) return true;
+        }
+    }
+};
