0b8b3188e0bc61c5c8c5266748b750f7cd63b165
hexiaoyu <hexi.aoyu@163.com>
2023-02-23 18:32:48 +0800

    C++ 0001

---
+++ src/main/cpp/hxy/03hash/00300001.cpp
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/23 17:59
+ * https://leetcode.cn/problems/two-sum/
+ * easy
+ */
+#include "unordered_map"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> flag;
+
+        vector<int> res;
+
+        for(int i = 0; i < nums.size(); i++) {
+            auto pivot = flag.find(target - nums[i]);
+            if(pivot == flag.end()){
+                flag[nums[i]] = i;
+            } else {
+                res.emplace_back(flag[target - nums[i]]);
+                res.emplace_back(i);
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+    vector<int> nums = {3,2,4};
+    int target = 6;
+
+    Solution *a = new Solution();
+    auto b = a->twoSum(nums, target);
+    return 0;
+}
