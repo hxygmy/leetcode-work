97c62d7e1f6ece6cf95ccb1e6147da082f37922a
hexiaoyu <hexi.aoyu@163.com>
2023-02-24 01:55:26 +0800

    C++ 0344

---
+++ src/main/cpp/hxy/04string/00340344.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/24 1:51
+ * https://leetcode.cn/problems/reverse-string/
+ * easy
+ */
+
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        char tmp;
+
+        while(left < right){
+            tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+};
