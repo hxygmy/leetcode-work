146fc1f0329e5e445ccbc6710df17d1ffcd398ea
hexiaoyu <hexi.aoyu@163.com>
2023-02-25 02:25:54 +0800

    C++ of05

---
+++ src/main/cpp/hxy/04string/0036of05.cpp
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/25 2:05
+ * https://leetcode.cn/problems/ti-huan-kong-ge-lcof/
+ * easy
+ */
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+        if(s.size() == 0) return "";
+
+        // 计算有几个空格
+        int num = 0;
+        for(char c : s){
+            if(c == ' ') {
+                num++;
+            }
+        }
+
+        int left = s.size() - 1;
+        s.resize(s.size() + 2 * num);
+        int right = s.size() - 1;
+
+        while(left < right){
+            if(s[left] == ' '){
+                s[right--] = '0';
+                s[right--] = '2';
+                s[right--] = '%';
+                left--;
+            } else {
+                s[right--] = s[left--];
+            }
+        }
+
+        return s;
+    }
+};
+
+int main(){
+    string s = "We are happy.";
+    Solution *a = new Solution();
+    a->replaceSpace(s);
+    return 0;
+}
