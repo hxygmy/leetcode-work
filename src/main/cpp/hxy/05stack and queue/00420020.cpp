a5ce97b590546af05522425ebe6f2d023be86e60
hexiaoyu <hexi.aoyu@163.com>
2023-02-26 16:13:17 +0800

    C++ 0020

---
+++ src/main/cpp/hxy/05stack and queue/00420020.cpp
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/26 16:02
+ * https://leetcode.cn/problems/valid-parentheses/
+ * easy
+ */
+#include "string"
+#include "stack"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> in;
+
+        for (char c: s) {
+            if (c == '[' or c == '{' or c == '(') {
+                in.push(c);
+            } else {
+                if (!in.empty()
+                    and (
+                            (c == '}' and in.top() == '{')
+                         or (c == ']' and in.top() == '[')
+                         or (c == ')' and in.top() == '(')
+                    )
+                ) {
+                    in.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return in.empty();
+    }
+};
