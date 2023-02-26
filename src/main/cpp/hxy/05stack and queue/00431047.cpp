033baa173af43033116c7ac71e7bf8e8b027f4f1
hexiaoyu <hexi.aoyu@163.com>
2023-02-26 16:33:56 +0800

    C++ 1047

---
+++ src/main/cpp/hxy/05stack and queue/00431047.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/26 16:14
+ * https://leetcode.cn/problems/remove-all-adjacent-duplicates-in-string/
+ * easy
+ */
+
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        for(char c : s){
+            if(res.empty()) {
+                res.push_back(c);
+            } else if (c != res.back()) {
+                res.push_back(c);
+            } else {
+                res.pop_back();
+            }
+        }
+
+        return res;
+    }
+};
