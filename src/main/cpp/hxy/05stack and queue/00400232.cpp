56a0bd0b132e747c069315fa1e62e84f258d8bb4
hexiaoyu <hexi.aoyu@163.com>
2023-02-26 15:36:22 +0800

    C++ 0232

---
+++ src/main/cpp/hxy/05stack and queue/00400232.cpp
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/2/26 15:17
+ * https://leetcode.cn/problems/implement-queue-using-stacks/
+ * easy
+ */
+#include "stack"
+
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> in;
+    stack<int> out;
+    MyQueue() {
+
+    }
+
+    void push(int x) {
+        in.push(x);
+    }
+
+    int pop() {
+        int p = peek();
+        if (p != -1) out.pop();
+        return p;
+    }
+
+    int peek() {
+        if(empty()){
+            return -1;
+        } else if (out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+
+    bool empty() {
+        return in.empty() and out.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
