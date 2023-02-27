cba5b2fd19a561eb5f30f865ef876b37268e08de
hexiaoyu <hexi.aoyu@163.com>
2023-02-27 18:11:06 +0800

    C++ 0347

---
+++ src/main/cpp/hxy/05stack and queue/00460347.cpp
@@ -0,0 +1,49 @@
+/*
+ * Created by hexiaoyu 
+ * on 2023/2/27 16:51
+ * https://leetcode.cn/problems/top-k-frequent-elements/
+ * medium
+ */
+
+#include <queue>
+#include "vector"
+#include "unordered_map"
+using namespace std;
+
+class Solution {
+public:
+    class mycomparison {
+    public:
+        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+            return lhs.second > rhs.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // 要统计元素出现频率
+        unordered_map<int, int> map; // map<nums[i],对应出现的次数>
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]]++;
+        }
+
+        // 对频率排序
+        // 定义一个小顶堆，大小为k
+        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> pri_que;
+
+        // 用固定大小为k的小顶堆，扫面所有频率的数值
+        for (unordered_map<int, int>::iterator it = map.begin(); it != map.end(); it++) {
+            pri_que.push(*it);
+            if (pri_que.size() > k) { // 如果堆的大小大于了K，则队列弹出，保证堆的大小一直为k
+                pri_que.pop();
+            }
+        }
+
+        // 找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
+        vector<int> result(k);
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pri_que.top().first;
+            pri_que.pop();
+        }
+        return result;
+
+    }
+};
