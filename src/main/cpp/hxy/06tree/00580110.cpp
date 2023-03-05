ba85d56e6dafacb29bc94ced7fc7cacb7efd4111
hexiaoyu <hexi.aoyu@163.com>
2023-03-05 17:26:25 +0800

    c++ 0110 平衡二叉树

---
+++ src/main/cpp/hxy/06tree/00580110.cpp
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/5 16:53
+ * https://leetcode.cn/problems/balanced-binary-tree/
+ * easy
+ */
+
+#include "queue"
+#include "algorithm"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) >= 0;
+    }
+};
+
