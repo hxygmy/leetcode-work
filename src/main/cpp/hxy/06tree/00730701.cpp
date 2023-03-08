fa467cbbcbca090c6504a6b477a35cd4d775e709
hexiaoyu <hexi.aoyu@163.com>
2023-03-08 21:14:43 +0800

    c++ 0701 插入二叉搜索树

---
+++ src/main/cpp/hxy/06tree/00730701.cpp
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/8 21:04
+ * https://leetcode.cn/problems/insert-into-a-binary-search-tree/
+ * medium
+ */
+
+#include "queue"
+#include "algorithm"
+#include "string"
+#include "stack"
+#include "unordered_map"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root) return new TreeNode(val);
+        if(root->val > val){
+            if(root->left){
+                insertIntoBST(root->left, val);
+            } else {
+                root->left = new TreeNode(val);
+            }
+        } else {
+            if(root->right){
+                insertIntoBST(root->right, val);
+            } else {
+                root->right = new TreeNode(val);
+            }
+        }
+        return root;
+    }
+};
