6a8fcd7ed25374fcf6a7fc32f39bce0fa128bcf7
hexiaoyu <hexi.aoyu@163.com>
2023-03-12 02:32:28 +0800

    c++ 0450 tmp

---
+++ src/main/cpp/hxy/06tree/00740450.cpp
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/8 21:15
+ * https://leetcode.cn/problems/delete-node-in-a-bst/
+ * medium
+ */
+#include "queue"
+#include "algorithm"
+#include "string"
+#include "stack"
+#include "unordered_map"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode* p = root;
+        while(p){
+            if(p->val == key){
+                break;
+            } else if(p->val > key){
+                p = p->left;
+            } else {
+                p = p->right;
+            }
+        }
+        if(!p) return nullptr;
+        if(p->left)
+    }
+};
