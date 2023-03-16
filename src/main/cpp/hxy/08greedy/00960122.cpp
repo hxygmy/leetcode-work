6f909c9c587a0187a6ce836ea349df93c181ea80
hexiaoyu <hexi.aoyu@163.com>
2023-03-17 03:33:41 +0800

    c++ 0122 买股票的最佳时机

---
+++ src/main/cpp/hxy/08greedy/00960122.cpp
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/17 3:21
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
+ * medium
+ */
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() < 2) return 0;
+
+        int res = 0;
+
+        for(int i = 0, j = 1; j < prices.size(); i++, j++){
+            if(prices[j] > prices[i]){
+                res += (prices[j] - prices[i]);
+            }
+        }
+        return res;
+    }
+};
