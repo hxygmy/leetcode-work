3d45e9e3769cb0cc83bb5ca4c1fedf01ed1dd86a
hexiaoyu <hexi.aoyu@163.com>
2023-03-16 23:57:31 +0800

    c++ 0053 最大子数组和

---
+++ src/main/cpp/hxy/08greedy/00950053.cpp
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/16 23:08
+ * https://leetcode.cn/problems/maximum-subarray/
+ * medium
+ */
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = -10001;
+        int sum = -10001;
+        int j = 0;
+        while(j < nums.size()){
+            if(sum < 0) {
+                sum = nums[j];
+            } else {
+                sum += nums[j];
+            }
+
+            if(res < sum) res = sum;
+            j++;
+        }
+
+        return res;
+    }
+};
