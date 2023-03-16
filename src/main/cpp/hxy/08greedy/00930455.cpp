9dc81c93da9be9b77713eccbf6cec9c7301b6ed0
hexiaoyu <hexi.aoyu@163.com>
2023-03-16 22:11:54 +0800

    c++ 0455 分发饼干 贪心第1题

---
+++ src/main/cpp/hxy/08greedy/00930455.cpp
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/16 1:59
+ * https://leetcode.cn/problems/assign-cookies/
+ * easy
+ */
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int res = 0;
+
+        int i = 0, j = 0;
+        while(i < g.size() and j < s.size()){
+            // 如果当前遍历的位置，s能满足g，就说明满足条件，如果满足不了，就让
+            if(g[i] <= s[j]){
+                res++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return res;
+    }
+};
