ec4566265738ee9b23c154f56beb19e13f86c1f8
hexiaoyu <hexi.aoyu@163.com>
2023-03-18 03:58:14 +0800

    c++ 0134 加油站

---
+++ src/main/cpp/hxy/08greedy/01000134.cpp
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/18 3:23
+ * https://leetcode.cn/problems/gas-station/
+ * medium
+ */
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int rest = 0;
+        int pos = 0;
+        int size = gas.size();
+        for(int i = 0; i < size; i++){
+            rest = rest + gas[i] - cost[i];
+            if(rest < 0){
+                pos = i + 1;
+                rest = 0;
+            }
+        }
+
+        // 模拟
+        int gases = 0;
+        for(int j = 0; j < size; j++){
+            gases = gases + gas[(j + pos) % size] - cost[(j + pos) % size];
+            if(gases < 0) return -1;
+        }
+
+        return pos;
+    }
+};
