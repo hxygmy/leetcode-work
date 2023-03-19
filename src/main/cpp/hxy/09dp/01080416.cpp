2953556700a7e816dfa5ae8197948dbe7b922832
hexiaoyu <hexi.aoyu@163.com>
2023-03-19 17:19:42 +0800

    c++ 0416 分割等和子集 01背包问题

---
+++ src/main/cpp/hxy/09dp/01080416.cpp
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by hexiaoyu 
+ * on 2023/3/18 22:05
+ * https://leetcode.cn/problems/partition-equal-subset-sum/
+ * medium
+ */
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+
+        vector<int> dp(10001, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
+
+        // 开始 01背包
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = target; j >= nums[i]; j--) {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        if (dp[target] == target) return true;
+        return false;
+    }
+};
