f3f8609fdbfaa159ce56c4938a50c3d999dd5aee
hexiaoyu <hexi.aoyu@163.com>
2023-04-15 05:34:57 +0800

    c++ 0912 经典排序，快排很难过用例

---
+++ src/main/cpp/hxy/01array/01110912.cpp
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by hexiaoyu
+ * on 2023/4/15 0:15
+ * https://leetcode.cn/problems/sort-an-array
+ * medium
+ */
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+
+    void quickSort(vector<int> &nums, int low, int high) {
+        if (low < high) {
+            int pivotpos = partition(nums, low, high);
+            quickSort(nums, low, pivotpos - 1);
+            quickSort(nums, pivotpos + 1, high);
+        }
+    }
+
+    int partition(vector<int> &nums, int low, int high) {
+        int mid = (low + high) / 2;
+        int pivot = nums[mid];
+        nums[mid] = nums[low];
+        nums[low] = pivot;
+        while (low < high) {
+            while (low < high && nums[high] >= pivot) {
+                high--;
+            }
+            nums[low] = nums[high];
+            while (low < high && nums[low] <= pivot) {
+                low++;
+            }
+            nums[high] = nums[low];
+        }
+        nums[low] = pivot;
+        return low;
+    }
+};
+
+int main(){
+    vector<int> x = {5,1,1,2,0,0};
+    Solution *a = new Solution();
+    a->sortArray(x);
+    int c = 0;
+    return c;
+}
