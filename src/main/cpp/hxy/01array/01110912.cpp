4a124445c32e171223a14502a895158fd34e0ffc
hexiaoyu <hexi.aoyu@163.com>
2023-04-25 15:34:47 +0800

    c++ 0912 经典排序，更新归并排序和堆排序

--- src/main/cpp/hxy/01array/01110912.cpp
+++ src/main/cpp/hxy/01array/01110912.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
+    vector<int> sortArrayQuickSort(vector<int>& nums) {
         quickSort(nums, 0, nums.size() - 1);
         return nums;
     }
@@ -60,12 +60,93 @@ public:
         nums[low] = pivot;
         return low;
     }
+
+    // 归并排序
+    vector<int> sortArrayMergeSort(vector<int>& nums) {
+        size = nums.size();
+        tmp.resize(size);
+        mergeSort(nums, 0, size - 1);
+        return nums;
+    }
+
+    int size;
+    vector<int> tmp;
+    void merge(vector<int> &nums, int low, int mid, int high) {
+        for(int i = low; i <= high; i++){
+            tmp[i] = nums[i];
+        }
+        int i, j, k;
+        for(i = low, j = mid + 1, k = i; i <= mid && j <= high; k++){
+            if(tmp[i] <= tmp[j]){
+                nums[k] = tmp[i++];
+            } else {
+                nums[k] = tmp[j++];
+            }
+        }
+        while(i <= mid){
+            nums[k++] = tmp[i++];
+        }
+        while(j <= high){
+            nums[k++] = tmp[j++];
+        }
+    }
+
+    void mergeSort(vector<int> &nums, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            merge(nums, low, mid, high);
+        }
+    }
+
+    // 堆排序
+    vector<int> sortArrayHeapSort(vector<int>& nums) {
+        heapSort(nums, nums.size());
+        return nums;
+    }
+
+    /**
+     * 把以k为根的子树调整为大根堆
+     * @param k 数组下标
+     * @param n 数组长度
+     */
+    void headAdjust(vector<int> &nums, int k, int n) {
+        int tmp = nums[k];
+        for (int i = 2 * k + 1; i < n; i = 2 * i + 1) {
+            if (i < n - 1 && nums[i] < nums[i + 1]) {
+                i++;
+            }
+            if (tmp < nums[i]) {
+                nums[k] = nums[i];
+                k = i;
+            } else {
+                break;
+            }
+        }
+        nums[k] = tmp;
+    }
+
+    void heapSort(vector<int> &nums, int n) {
+        for (int i = n / 2; i >= 0; i--) {
+            headAdjust(nums, i, n);
+        }
+
+        int swap;
+        for (int i = n - 1; i >= 0; i--) {
+            swap = nums[i];
+            nums[i] = nums[0];
+            nums[0] = swap;
+
+            headAdjust(nums, 0, i);
+        }
+    }
 };
 
 int main(){
     vector<int> x = {5,1,1,2,0,0};
     Solution *a = new Solution();
-    a->sortArray(x);
+    a->sortArrayHeapSort(x);
     int c = 0;
     return c;
 }
